d5d4213f9c55a21bc899d425b60fb729350a1d20
kmafeni04 <komemafeni944@gmail.com>
2024-12-13 11:11:32 +0100

    Beginning of handling renaming of DotIndex variables

--- server/rename.lua
+++ server/rename.lua
@@ -1,3 +1,5 @@
+local switch = require("lib.switch")
+
 local response = require("utils.response")
 local logger = require("utils.logger")
 local find_nodes = require("utils.find_nodes")
@@ -53,41 +55,111 @@ return function(request_id, request_params, current_file, ast)
   local found_nodes, err = find_nodes(current_file, position.line, position.character, ast)
   if found_nodes then
     local current_node = found_nodes[#found_nodes]
-    if current_node.is_Id or current_node.is_IdDecl then
-      local scope_node = current_node.attr.scope.node
-      local scope_nodes, err = find_nodes_in_scope_node(scope_node, scope_node.endpos, {})
-      if scope_nodes then
-        for _, node in ipairs(scope_nodes) do
-          if
-            (node.is_Id or node.is_IdDecl)
-            and node.pos
-            and node.attr
-            and node.attr.name
-            and node.attr.name == current_node.attr.name
-            and node.attr.type
-            and tostring(node.attr.type) == tostring(current_node.attr.type)
-          then
-            local s_line, s_char = pos_to_line_and_char(node.pos, current_file)
-            table.insert(changes[request_params.textDocument.uri], {
-              ---@type Range
-              range = {
-                start = {
-                  line = s_line,
-                  character = s_char,
+    switch(true, {
+      [(current_node.is_Id or current_node.is_IdDecl) or false] = function()
+        local scope_node = current_node.attr.scope.node
+        local scope_nodes, err = find_nodes_in_scope_node(scope_node, scope_node.endpos, {})
+        if scope_nodes then
+          for _, node in ipairs(scope_nodes) do
+            if
+              (node.is_Id or node.is_IdDecl)
+              and node.pos
+              and node.attr
+              and node.attr.name
+              and node.attr.name == current_node.attr.name
+              and node.attr.type
+              and tostring(node.attr.type) == tostring(current_node.attr.type)
+            then
+              local s_line, s_char = pos_to_line_and_char(node.pos, current_file)
+              table.insert(changes[request_params.textDocument.uri], {
+                ---@type Range
+                range = {
+                  start = {
+                    line = s_line,
+                    character = s_char,
+                  },
+                  ["end"] = {
+                    line = s_line,
+                    character = s_char + #current_node.attr.name,
+                  },
                 },
-                ["end"] = {
-                  line = s_line,
-                  character = s_char + #current_node.attr.name,
-                },
+                newText = request_params.newName,
+              })
+            end
+          end
+        else
+          logger.log(err)
+        end
+      end,
+      [current_node.is_DotIndex or false] = function()
+        --TODO: Handle scope nodes
+        if current_node[2] and type(current_node[2]) == "table" then
+          -- local scope_node = current_node[2].attr.scope.node
+          -- local scope_nodes, err = find_nodes_in_scope_node(scope_node, scope_node.endpos, {})
+          local s_line, s_char = pos_to_line_and_char(current_node.pos + 1, current_file)
+          table.insert(changes[request_params.textDocument.uri], {
+            ---@type Range
+            range = {
+              start = {
+                line = s_line,
+                character = s_char,
               },
-              newText = request_params.newName,
-            })
+              ["end"] = {
+                line = s_line,
+                character = s_char + #current_node[1],
+              },
+            },
+            newText = request_params.newName,
+          })
+          local parent_node = current_node[2]
+          if parent_node.attr.type.is_record or parent_node.attr.type.is_union then
+            for _, node_fields in ipairs(parent_node.attr.type.node) do
+              if type(node_fields) == "table" then
+                if node_fields[1] == current_node[1] then
+                  local s_line, s_char = pos_to_line_and_char(node_fields.pos, current_file)
+                  table.insert(changes[request_params.textDocument.uri], {
+                    ---@type Range
+                    range = {
+                      start = {
+                        line = s_line,
+                        character = s_char,
+                      },
+                      ["end"] = {
+                        line = s_line,
+                        character = s_char + #current_node[1],
+                      },
+                    },
+                    newText = request_params.newName,
+                  })
+                  break
+                end
+              end
+            end
+          elseif parent_node.attr.value then
+            for _, node_fields in ipairs(parent_node.attr.value.node[2]) do
+              if type(node_fields) == "table" then
+                local s_line, s_char = pos_to_line_and_char(node_fields.pos, current_file)
+                table.insert(changes[request_params.textDocument.uri], {
+                  ---@type Range
+                  range = {
+                    start = {
+                      line = s_line,
+                      character = s_char,
+                    },
+                    ["end"] = {
+                      line = s_line,
+                      character = s_char + #current_node[1],
+                    },
+                  },
+                  newText = request_params.newName,
+                })
+                break
+              end
+            end
           end
         end
-      else
-        logger.log(err)
-      end
-    end
+      end,
+    })
   else
     logger.log(err)
   end
