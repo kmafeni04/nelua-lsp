3f4d3e88e0f85e99eed27b97b6db03ca07654dd7
kmafeni04 <komemafeni944@gmail.com>
2024-12-15 23:51:32 +0100

    Remove unnecessary code from analyzing the ast

--- utils/analyze_ast.lua
+++ utils/analyze_ast.lua
@@ -1,6 +1,5 @@
 local except = require("nelua.utils.except")
 local analyzer = require("nelua.analyzer")
-local typedefs = require("nelua.typedefs")
 local AnalyzerContext = require("nelua.analyzercontext")
 local aster = require("nelua.aster")
 local generator = require("nelua.cgenerator")
@@ -15,21 +14,14 @@ return function(current_file, current_file_path)
   local ast
   local ok, err = except.trycall(function()
     ast = aster.parse(current_file, current_file_path)
-    -- logger.log(ast)
     local context = AnalyzerContext(analyzer.visitors, ast, generator)
     except.try(function()
-      for _, v in pairs(typedefs.primtypes) do
-        if v.metafields then
-          v.metafields = {}
-        end
-      end
-
       context = analyzer.analyze(context)
+      -- collectgarbage()
     end, function(e)
       e.message = ("%s%s"):format(context:get_visiting_traceback(1), e:get_message())
     end)
   end)
-  -- logger.log(ast)
   if ok then
     return ast, nil
   else
